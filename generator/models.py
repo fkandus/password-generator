1fb4e3033eaa24798644f4d193766caea9e822d2
Federico Kandus <fkandus@gmail.com>
2025-02-18 17:20:52 +0100

    Password generator

--- generator/models.py
+++ generator/models.py
@@ -1,7 +1,28 @@
+import random
+import string
 from django.db import models
 
 
 # Create your models here.
 class Password(models.Model):
     content = models.CharField(max_length=50)
-    created_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def generate_password_and_save(
+        self, length, use_lowercase, use_uppercase, use_digits, use_special
+    ):
+        characters = ""
+
+        if use_lowercase:
+            characters = string.ascii_lowercase
+        if use_uppercase:
+            characters += string.ascii_uppercase
+        if use_digits:
+            characters += string.digits
+        if use_special:
+            characters += string.punctuation
+
+        self.content = "".join(random.choice(characters) for _ in range(length))
+        self.save()
+
+        return self.content
--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,42 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from generator.models import Password
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the generator index.")
+    context = {}
+
+    if request.method == "POST":
+        length = request.POST.get("length", None)
+        uppercase = request.POST.get("uppercase", None)
+        lowercase = request.POST.get("lowercase", None)
+        numbers = request.POST.get("numbers", None)
+        special = request.POST.get("special", None)
+
+        context = {
+            "length": length,
+            "uppercase": uppercase,
+            "lowercase": lowercase,
+            "numbers": numbers,
+            "special": special,
+        }
+
+        if length == None:
+            context["error"] = "Please enter a length."
+        elif (
+            uppercase == None
+            and lowercase == None
+            and numbers == None
+            and special == None
+        ):
+            context["error"] = "Please select at least one option."
+        else:
+            context["password"] = Password().generate_password_and_save(
+                int(length),
+                lowercase,
+                uppercase,
+                numbers,
+                special,
+            )
+
+    return render(request, "generator/index.html", context)
