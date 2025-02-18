9344bc9078202ea942518cf5efc41f0a10edcb7b
Federico Kandus <fkandus@gmail.com>
2025-02-18 18:17:16 +0100

    Added CSS styles and Copy to Clipboard

---
+++ generator/static/generator/scripts.js
@@ -0,0 +1,14 @@
+function copyText() {
+    var copyText = document.getElementById("password");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied!";
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
